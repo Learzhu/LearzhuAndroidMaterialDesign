933e1d469d9bc5d1dfe9440e24770953546ddc1a
Learzhu <694125196@qq.com>
2016-08-22 09:43:12 +0800

    MaterialDesign Demo

--- app/src/main/java/com/learzhu/learzhuandroidmaterialdesign/MainActivity.java
+++ app/src/main/java/com/learzhu/learzhuandroidmaterialdesign/MainActivity.java
@@ -1,21 +1,36 @@
 package com.learzhu.learzhuandroidmaterialdesign;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    /*侧边栏*/
+    /*全部的布局 有侧边栏效果*/
     private DrawerLayout mDrawerLayout;
-
+    /*侧边栏*/
     private NavigationView navigationView;
+
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +53,26 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        /*底部的悬浮按钮*/
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(findViewById(R.id.drawer_layout), "I'm a Snackbar", Snackbar.LENGTH_LONG).setAction("Action", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Snackbar Action", Toast.LENGTH_LONG).show();
+                    }
+                }).show();
+            }
+        });
+
+        /*加入Fragment*/
+        DesignDemoPagerAdapter pagerAdapter = new DesignDemoPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(pagerAdapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -62,4 +97,60 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    /*Fragment*/
+    public static class DesignDemoFragment extends Fragment {
+        private static final String TAB_POSITION = "tab_position";
+
+        public DesignDemoFragment() {
+        }
+
+        public static DesignDemoFragment newInstance(int tabPositon) {
+            DesignDemoFragment designDemoFragment = new DesignDemoFragment();
+            Bundle args = new Bundle();
+            args.putInt(TAB_POSITION, tabPositon);
+            designDemoFragment.setArguments(args);
+            return designDemoFragment;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            Bundle args = getArguments();
+            int tabPositon = args.getInt(TAB_POSITION);
+            TextView textView = new TextView(getActivity());
+            textView.setGravity(Gravity.CENTER);
+            textView.setText("Text in Tab # " + tabPositon);
+//            ArrayList<String> items = new ArrayList<>();
+//            for (int i = 0; i < 50; i++) {
+//                items.add("Tab #" + tabPositon + " item #" + i);
+//            }
+//            View view=inflater.inflate(R.layout.f)
+            return textView;
+        }
+    }
+
+    /*ViewPager的Adapter*/
+    static class DesignDemoPagerAdapter extends FragmentStatePagerAdapter {
+
+        public DesignDemoPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return DesignDemoFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tab " + position;
+        }
+    }
+
 }
